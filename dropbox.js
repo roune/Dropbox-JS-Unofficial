726114103f9506810310d62f2cfac4a3579b694e
Your Name <you@example.com>
2016-01-27 16:47:46 +0100

    Translated to pure JS. Works without any aditional library.

--- dropbox.js
+++ dropbox.js
@@ -1,145 +1,176 @@
-function get_personal_info(token, done) {
-	$.ajax({
-		url: "https://api.dropboxapi.com/2/users/get_current_account?authorization=Bearer " + token,
-		type: 'POST',
-		success: function(data) {
-			done(null, data)
-		},
-		error: function(error) {
-			done(error, null);
+var Dropbox = function(CLIENT_ID, redirect_uri) {
+	this.token;
+	
+	//CLIENT_ID = 9slwxgfxcbmjxh9
+	//redirect_uri = https://localhost
+	
+	this.href = "https://www.dropbox.com/1/oauth2/authorize?response_type=token&client_id=" + CLIENT_ID + "&redirect_uri=" + redirect_uri;
+
+	/**
+	 * Mejorar auth method
+	 */
+	this.setUpDropbox = function () {
+		var cookies = document.cookie;
+		if (cookies != "") {
+			console.log('already logged');
+			this.token = cookies.slice(cookies.indexOf('=') + 1);
+			return true;
+		} else if (window.location.toString().search("access_token")!=-1){
+			access_token = window.location.toString().split('#')[1].split('&')[0].split('=')[1];
+			document.cookie ="dropbox_id="+access_token;
+			this.token = access_token;
+			console.log("just logged");
+			return true;
+		} else {
+			console.log("no logged");
+			return false;
 		}
-	})
-}
-
-
-
-function list_folder (token, path, recursive, include_media_info, include_deleted, done) {
-   
-	final_url = "https://api.dropboxapi.com/2/files/list_folder?authorization=Bearer " + token;
-
-	var data = {
-		recursive:recursive || false,
-		include_media_info:include_media_info || false,
-		include_deleted:include_deleted || false,
-		path:path || ""
-	};
-
-	$.ajax({
-		url: final_url,
-		type: 'POST',
-		contentType: "application/json",
-		success: function(data) {
-			console.log(data);
-			done(null, data);
-		},
-		dataType: "json",
-		data: JSON.stringify(data),
-		error: function(error) {
-			done(error, null);
-		}
-	})
-}
-
-function get_shared_link (token, path, done) {
-
-	if (path == null) {
-		done("Not file or folder given", null);
 	}
 
-	var url = "https://api.dropboxapi.com/2/sharing/create_shared_link_with_settings?authorization=Bearer " + token;
-
-	var data = {
-		path: "/Comenzar.pdf",
+	this.get_personal_info = function (done) {
+		var xhr = new XMLHttpRequest();
+		var url = "https://api.dropboxapi.com/2/users/get_current_account?authorization=Bearer " + this.token;
+		xhr.open("POST", url, true);
+		xhr.onreadystatechange = function () { 
+		    if (xhr.readyState == 4) {
+		    	if (xhr.status == 200) {
+		    		var json = JSON.parse(xhr.responseText);
+		        	done(null, json);
+		    	} else {
+		    		done(xhr.statusText);
+		    	}
+		    }
+		}
+		xhr.send();
 	}
 
-	$.ajax({
-		url: url,
-		type: 'POST',
-		contentType: "application/json",
-		success: function(data) {
-			console.log(data);
-			done(null, data);
-		},
-		dataType: "json",
-		data: JSON.stringify(data),
-		error: function(error) {
-			console.log(error);
-			done(error, null);
-		}
-	})
-}
-
-function list_shared_links(token, done) {
-
-	var url = "https://api.dropboxapi.com/2/sharing/list_shared_links?authorization=Bearer " + token;
-
-	var data = {
+	this.list_folder = function (path, recursive, include_media_info, include_deleted, done) {
 		
+		var data = {
+			recursive:recursive || false,
+			include_media_info:include_media_info || false,
+			include_deleted:include_deleted || false,
+			path:path || ""
+		};
+
+		xhr = new XMLHttpRequest();
+		var url = "https://api.dropboxapi.com/2/files/list_folder?authorization=Bearer " + this.token;
+		xhr.open("POST", url, true);
+		xhr.setRequestHeader("Content-type", "application/json");
+		xhr.onreadystatechange = function () { 
+		    if (xhr.readyState == 4) {
+		    	if (xhr.status == 200) {
+		    		var json = JSON.parse(xhr.responseText);
+		        	done(null, json);
+		    	} else {
+		    		done(xhr.statusText);
+		    	}
+		    }
+		}
+		data = JSON.stringify(data);
+		xhr.send(data);
 	}
 
-	$.ajax({
-		url: url,
-		type: 'POST',
-		contentType: "application/json",
-		success: function(data) {
-			console.log(data);
-			done(null, data);
-		},
-		dataType: "json",
-		data: JSON.stringify(data),
-		error: function(error) {
-			console.log(error);
-			done(error, null);
+	this.get_shared_link = function (path, done) {
+
+		if (path == null) {
+			done("Not file or folder given", null);
 		}
-	})
-}
 
-function get_metadata (token, file, include_media_info, done) {
-
-	var url = "https://api.dropboxapi.com/2/files/get_metadata?authorization=Bearer " + token;
-
-	var data = {
-		path: file || "",
-		include_media_info: include_media_info || false
-	};
-
-	$.ajax({
-		url: url,
-		type: 'POST',
-		contentType: "application/json",
-		data: JSON.stringify(data),
-		success: function(data) {
-			console.log(data);
-			done(null, data);
-		},
-		error: function(error) {
-			console.log(error);
-			done(error, null);
+		var data = {
+			path: path,
 		}
-	})
-}
 
-function download_shared_link(token, url, done) {
-   
-	final_url = "https://content.dropboxapi.com/2/sharing/get_shared_link_file?authorization=Bearer " + token;
-
-	var data = {
-		url: "https://www.dropbox.com/sh/8pesxq81iegb1yi/AAAkFX6Nq7epChDA2IT-4_hra?dl=0",
-	};
-
-	$.ajax({
-		url: final_url,
-		type: 'POST',
-		success: function(data) {
-			console.log(data);
-			done(null, data);
-		},
-		headers: {
-			"Dropbox-API-Arg":JSON.stringify(data)
-		},
-		error: function(error) {
-			console.log(error);
-			done(error, null);
+		xhr = new XMLHttpRequest();
+		var url = "https://api.dropboxapi.com/2/sharing/create_shared_link_with_settings?authorization=Bearer " + this.token;
+		xhr.open("POST", url, true);
+		xhr.setRequestHeader("Content-type", "application/json");
+		xhr.onreadystatechange = function () { 
+		    if (xhr.readyState == 4) {
+		    	if (xhr.status == 200) {
+		    		var json = JSON.parse(xhr.responseText);
+		        	done(null, json);
+		    	} else {
+		    		done(xhr.statusText);
+		    	}
+		    }
 		}
-	})	
+		data = JSON.stringify(data);
+		xhr.send(data);
+	}
+
+	this.list_shared_links = function (done) {
+
+		var data = {
+			
+		}
+
+		xhr = new XMLHttpRequest();
+		var url = "https://api.dropboxapi.com/2/sharing/list_shared_links?authorization=Bearer " + this.token;
+		xhr.open("POST", url, true);
+		xhr.setRequestHeader("Content-type", "application/json");
+		xhr.onreadystatechange = function () { 
+		    if (xhr.readyState == 4) {
+		    	if (xhr.status == 200) {
+		    		var json = JSON.parse(xhr.responseText);
+		        	done(null, json);
+		    	} else {
+		    		done(xhr.statusText);
+		    	}
+		    }
+		}
+		data = JSON.stringify(data);
+		xhr.send(data);
+	}
+
+	this.get_metadata = function (file, include_media_info, done) {
+
+		var url = "https://api.dropboxapi.com/2/files/get_metadata?authorization=Bearer " + this.token;
+		var data = {
+			path: file || "",
+			include_media_info: include_media_info || false
+		};
+
+		xhr = new XMLHttpRequest();
+		xhr.open("POST", url, true);
+		xhr.setRequestHeader("Content-type", "application/json");
+		xhr.onreadystatechange = function () { 
+		    if (xhr.readyState == 4) {
+		    	if (xhr.status == 200) {
+		    		var json = JSON.parse(xhr.responseText);
+		        	done(null, json);
+		    	} else {
+		    		done(xhr.statusText);
+		    	}
+		    }
+		}
+		data = JSON.stringify(data);
+		xhr.send(data);
+	}
+
+	this.download_shared_link = function (url, done) {
+	   
+		url = "https://content.dropboxapi.com/2/sharing/get_shared_link_file?authorization=Bearer " + this.token;
+
+		var data = {
+			url: url,
+		};
+
+		xhr = new XMLHttpRequest();
+		xhr.open("POST", url, true);
+		xhr.setRequestHeader("Content-type", "application/json");
+		xhr.onreadystatechange = function () { 
+		    if (xhr.readyState == 4) {
+		    	if (xhr.status == 200) {
+		    		var json = JSON.parse(xhr.responseText);
+		        	done(null, json);
+		    	} else {
+		    		done(xhr.statusText);
+		    	}
+		    }
+		}
+		data = JSON.stringify(data);
+		xhr.send(data);
+	}
+
 }
